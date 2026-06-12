a325a7af59924220d6ac6a4e7fab70e129bb8b3f
XTFYyayy <3362852920@qq.com>
2026-06-12 15:16:15 +0800

    Add files via upload

---
+++ DocCode/Extensions/CardExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using Doc.DocCode.Attributes;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Doc.DocCode.Extensions;
+
+public static class CardExtensions
+{
+    private static CardTagsAttribute? GetCardTagsAttribute(this CardModel card)
+    {
+        var type = card.GetType();
+        return Attribute.GetCustomAttribute(type, typeof(CardTagsAttribute)) as CardTagsAttribute;
+    }
+
+    public static bool IsKnight(this CardModel card)
+    {
+        var attr = card.GetCardTagsAttribute();
+        return attr != null && attr.IsKnight;
+    }
+
+    public static bool IsKazimierz(this CardModel card)
+    {
+        var attr = card.GetCardTagsAttribute();
+        return attr != null && attr.IsKazimierz;
+    }
+}
